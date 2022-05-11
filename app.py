20ab32145cfe7c92e7e2eeb633bd9b83b5bf8be1
southoftheriver <anwjsrlrhwkd@naver.com>
2022-05-11 14:16:49 +0900

    Update app.py 네이버 API 설정

--- app.py
+++ app.py
@@ -20,6 +20,24 @@ client = MongoClient('mongodb+srv://anwjsrlrhwkd:spa0000@cluster0.imdsh.mongodb.
 db = client.WebMiniProject
 
 # 서버 기능 구현
+def get_wishlist():
+    # 네이버 API 설정
+    client_id = "coSd7QD9SiS6WMyg3bx2"
+    client_secret = "gm90HNggug"
+    headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret}
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    display = "20"
+    query = "아이패드 11"
+    api_url = "https://openapi.naver.com/v1/search/shop.json?query="+query+"&display="+display
+
+    r = requests.get(api_url, headers=headers)
+    response = r.json()
+
+    for data in response["items"]:
+        print(data['title'][:30])
+        print(data["lprice"], "원")
+
+
+
+# if __name__ == '__main__':
+#     app.run('0.0.0.0', port=5000, debug=True)

429b281a80b284b22080c9086bf5571f129f370a
jkim9409 <jkimnyc2020@gmail.com>
2022-05-12 14:51:56 +0900

    edit index.html and app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from pymongo import MongoClient
 import jwt
 import datetime
 import hashlib
+import requests
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
-
+import re
 
 
 app = Flask(__name__)
@@ -77,7 +78,17 @@ def sign_up():
         "nick": username_receive,  # 프로필 이름 기본값은 아이디
         "img": "",  # 프로필 사진 파일 이름
         "img_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
-        "description": ""  # 프로필 한 마디
+        "description": "", # 자기 소개
+        "address": "", # 주소
+        "age": "", # 나이
+        "dream": "", # 장래희망
+        "email": "", # 이메일
+        "hobby": "", # 취미
+        "job": "", # 직업
+        "insta": "",  # 인스타
+        "phone_number": "",  # 연락처
+
+
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -88,5 +99,122 @@ def check_dup():
     exists = bool(db.users.find_one({"user_id": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
+
+# html 태그 제거
+def remove_tag(string):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', string)
+    return cleantext
+
+# 상품정보 저장
+def get_wishlist(query):
+    # 네이버 API 설정
+    client_id = "coSd7QD9SiS6WMyg3bx2"
+    client_secret = "gm90HNggug"
+    headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret}
+
+    display = "1"
+    api_url = "https://openapi.naver.com/v1/search/shop.json?query="+query+"&display="+display
+
+    r = requests.get(api_url, headers=headers)
+    response = r.json()
+    data = response["items"][0]
+
+    name = remove_tag(data['title'])
+    img_url = data["image"]
+    price = int(data["lprice"])
+
+    return name, img_url, price
+
+
+@app.route('/posting', methods=['POST'])
+def posting():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"user_id": payload["id"]})
+
+        name, img_url, price = get_wishlist(request.form["product_give"])
+        date_receive = request.form["date_give"]
+        is_public_receive = request.form["is_public"]
+
+        doc = {
+            "user_id": user_info["user_id"],
+            "name": name,
+            "img_url": img_url,
+            "price": price,
+            "upload_date": date_receive,
+            "purchashed": False,
+            "deleted": False,
+            "public": is_public_receive == "true"
+        }
+        db.products.insert_one(doc)
+        return jsonify({"result": "success", 'msg': '포스팅 성공'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route("/get_public_posts", methods=['GET'])
+def get_public_posts():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        products = list(db.products.find({"public": True}).sort("date", -1).limit(20))
+
+        for product in products:
+            product["_id"] = str(product["_id"])
+            product["likes"] = db.likes.count_documents({"prod_id": product["_id"]})
+            product["likes_by_me"] = bool(
+                db.likes.find_one({"prod_id": product["_id"], "user_id": payload['id']}))
+
+
+        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다.", "products": products})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"user_id": username}, {"_id": False})
+        return render_template('mywishlist.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+@app.route('/update_like', methods=['POST'])
+def update_like():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"user_id": payload["id"]})
+        prod_id_receive = request.form["prod_id_give"]
+        action_receive = request.form["action_give"]
+        doc = {
+            "prod_id": prod_id_receive,
+            "user_id": user_info["user_id"],
+        }
+        if action_receive == "like":
+            db.likes.insert_one(doc)
+        else:
+            db.likes.delete_one(doc)
+
+        count = db.likes.count_documents({"prod_id": prod_id_receive})
+        return jsonify({"result": "success", 'msg': 'updated', "count": count})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
         app.run('0.0.0.0', port=5000, debug=True)
+
